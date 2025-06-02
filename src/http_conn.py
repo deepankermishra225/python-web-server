580d1c4fcfd0df237b23637852f15ecab22493cf
Deepanker Mishra <dmishra60@gatech.edu>
2025-06-03 02:05:50 +0530

    added signal handling class

---
+++ src/http_conn.py
@@ -0,0 +1,101 @@
+"""
+asyncio provides a higher level StreamReader/StreamWriter API for working with streams, which is often more convenient than using Protocol and Transport directly.
+However, since asyncio.Protocol and Transport provide more control over the connection and data flow, they are often used in more complex scenarios where you need to manage the connection state or implement custom protocols.
+
+When using asyncio.Protocol, requried methods include:
+- connection_made(transport): Called when a connection is made. The transport object represents the connection.
+- data_received(data): Called when data is received. The data parameter contains the bytes received.
+- connection_list(exc): Called when the connection is closed or list exc is an Exception or (None).
+- [OPTIONAL]connection_lost(exc): Called when the connection is lost. The exc parameter contains the exception if the connection was closed with an error.
+- eof_recieved(): Called when the end of the file (EOF) is received. This is typically used to handle the end of a stream.
+- pause_writing(): Called when the transport's buffer goes over a high water mark
+- resume_writing(): Called when the transport's buffer goes below a low water mark
+"""
+import logging
+import asyncio
+from typing import Literal
+from .flow_control import FlowControl
+
+logger = logging.getLogger(__name__)
+
+class HTTPConn(asyncio.Protocol):
+
+    def __init__(self, 
+                 app, 
+                 loop: asyncio.AbstractEventLoop | None = None):
+        self.loop = loop or asyncio.get_event_loop()
+        self.app = app
+        self.transport: asyncio.Transport | None = None
+        self.flow_control: FlowControl| None = None
+        self.connections = []
+        self.client: tuple[str, int] | None = None
+        self.server: tuple[str, int] | None = None
+        self.scheme = Literal["http", "https"] | None = None
+
+    def connection_made(self, transport: asyncio.Transport):
+        self.connections.append(self)
+        self.transport = transport
+        self.flow_control = FlowControl(transport)
+        logger.debug(f"Connection made with {transport.get_extra_info('peername')}")
+
+        transport.set_write_buffer_limits(
+            high=transport.get_write_buffer_limits()[1],
+            low=transport.get_write_buffer_limits()[0]
+        )
+
+    def data_received(self, data: bytes):
+        logger.debug(f"Data received: {data.decode()}")
+        asyncio.create_task(self._handle_data(data))
+        
+
+    async def _handle_data(self, data: bytes):
+        logger.debug(f"Handling data: {data.decode()}")
+        if self.flow_control:
+            await self.flow_control.drain()
+        if self.transport:
+            self.transport.write(data)
+            logger.debug(f"Data sent: {data.decode()}")
+
+    def pause_writing(self):
+        """
+        Called by transport when the write buffer exceeds the high water mark
+        """
+        logger.debug("Pausing reading")
+        if self.flow_control:
+            self.flow_control.pause_writing()
+
+    def resume_writing(self):
+        """
+        Called by the transport when the write buffer falls below the low water mark
+        """
+        logger.debug("Resuming reading")
+        if self.flow_control:
+            self.flow_control.resume_writing()
+
+    def connection_lost(self, exc: Exception | None):
+        logger.debug(f"Connection lost: {exc}")
+        self.transport = None
+        self.flow_control = None    
+        self.connections.discard(self)
+        if exc:
+            logger.error(f"Connection lost with error: {exc}")
+        else:
+            logger.info("Connection closed gracefully")
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    loop = asyncio.get_running_loop()
+
+    server = await loop.create_server(
+        lambda: Server(),
+        host='127.0.0.1',
+        port=8081
+    )
+    logger.info("Server is ready!")
+    async with server:
+        await server.serve_forever()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- src/server.py
+++ src/server.py
@@ -1,75 +1,78 @@
-"""
-asyncio provides a higher level StreamReader/StreamWriter API for working with streams, which is often more convenient than using Protocol and Transport directly.
-However, since asyncio.Protocol and Transport provide more control over the connection and data flow, they are often used in more complex scenarios where you need to manage the connection state or implement custom protocols.
-
-When using asyncio.Protocol, requried methods include:
-- connection_made(transport): Called when a connection is made. The transport object represents the connection.
-- data_received(data): Called when data is received. The data parameter contains the bytes received.
-- connection_list(exc): Called when the connection is closed or list exc is an Exception or (None).
-- [OPTIONAL]connection_lost(exc): Called when the connection is lost. The exc parameter contains the exception if the connection was closed with an error.
-- eof_recieved(): Called when the end of the file (EOF) is received. This is typically used to handle the end of a stream.
-- pause_writing(): Called when the transport's buffer goes over a high water mark
-- resume_writing(): Called when the transport's buffer goes below a low water mark
-"""
-import logging
+from typing import TYPE_CHECKING, Generator
 import asyncio
-from .flow_control import FlowControl
+import signal
+import sys
+import logging
+import contextlib
+import threading
+
+if TYPE_CHECKING:
+    from .http_conn import HTTPConn
+
+
+HANDLED_SIGNALS = {
+    signal.SIGINT: "SIGINT",
+    signal.SIGTERM: "SIGTERM",
+}
+if sys.platform == "win32":
+    HANDLED_SIGNALS[signal.SIGBREAK] = "SIGBREAK"
+
 
 logger = logging.getLogger(__name__)
 
-class Server(asyncio.Protocol):
-
+class ServerState:
+    """
+    Shared servers state that is available b/w all protocol instances (from uvicorn)
+    """
     def __init__(self):
-        self.transport: asyncio.Transport | None = None
-        self.flow_control = None
+        self.total_requests = 0
+        self.connections: set[HTTPConn] = set()
+        self.tasks: set[asyncio.Task[None]] = set()
+        self.default_headers = list[tuple[bytes, bytes]] = []
 
-    def connection_made(self, transport: asyncio.Transport):
-        self.transport = transport
-        self.flow_control = FlowControl(transport)
-        logger.debug(f"Connection made with {transport.get_extra_info('peername')}")
 
-        transport.set_write_buffer_limits(
-            high=transport.get_write_buffer_limits()[1],
-            low=transport.get_write_buffer_limits()[0]
-        )
 
-    def data_recieved(self, data: bytes):
-        logger.debug(f"Data received: {data.decode()}")
-        self.flow_control.drain()
-        # Echo the data back to the client
-        if self.transport:
-            self.transport.write(data)
-            logger.debug(f"Data sent: {data.decode()}")
+class Server:
+    def __init__(self):
+        self.server_state = ServerState()
+        self.started = False
+        self.should_exit = False
+        self.force_exit = False
+        self._captured_signals: list[int] = []
+    
+    def run(self) -> None:
+        return asyncio.run(self.serve())
+    
+    async def serve(self) -> None:
+        with self.capture_signals():
+            await self._serve()
 
-    def pause_writing(self):
+    async def _serve(self):
+        ...
+
+    # signal handling
+    @contextlib.contextmanager
+    def capture_signals(self) -> Generator[None, None, None]:
         """
-        Called by transport when the write buffer exceeds the high water mark
+        Signals can only be listened to from the main thread
         """
-        logger.debug("Pausing reading")
-        if self.flow_control:
-            self.flow_control.pause_writing()
+        if threading.current_thread() is not threading.main_thread():
+            yield
+            return
+        original_handlers = {sig: signal.signal(sig, self.handle_exit) for sig in HANDLED_SIGNALS.keys()}
+        try:
+            yield
+        finally:
+            # Restore original signal handlers
+            for sig, handler in original_handlers.items():
+                signal.signal(sig, handler)
+            # Raise captured signals in reverse order to ensure proper handling
+            for captured_signal in reversed(self._captured_signals):
+                signal.raise_signal(captured_signal)
 
-    def resume_writing(self):
-        """
-        Called by the transport when the write buffer falls below the low water mark
-        """
-        logger.debug("Resuming reading")
-        if self.flow_control:
-            self.flow_control.resume_writing()
-
-
-async def main():
-    loop = asyncio.get_running_loop()
-
-    server = await loop.create_server(
-        lambda: Server(),
-        host='127.0.0.1',
-        port=8081
-    )
-    logger.info("Server is ready!")
-    async with server:
-        await server.serve_forever()
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    def handle_exit(self, sig: int, frame: signal.FrameType | None) -> None:
+        self._captured_signals.append(sig)
+        if self.should_exit and sig==signal.SIGINT:
+            self.force_exit = True
+        else:
+            self.should_exit = True
